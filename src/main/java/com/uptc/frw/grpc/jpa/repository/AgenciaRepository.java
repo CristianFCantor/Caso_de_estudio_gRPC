d5e48e360e002388807ef44e9884caa9a181d2aa
Cristian <cristian.cantor01@uptc.com>
2024-11-13 21:36:08 -0500

    inicio

---
+++ src/main/java/com/uptc/frw/grpc/jpa/repository/AgenciaRepository.java
@@ -0,0 +1,7 @@
+package com.uptc.frw.grpc.jpa.repository;
+
+import com.uptc.frw.grpc.jpa.models.Agencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgenciaRepository extends JpaRepository<Agencia, Long> {
+}
